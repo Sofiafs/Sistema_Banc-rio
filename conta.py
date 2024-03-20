4a071a2b5d0e730bd68f5c0c79d4e90756533512
Melissa <m3lgaldin0@gmail.com>
2024-03-20 14:41:43 -0300

    primeiro commit da classe conta

--- conta.py
+++ conta.py
@@ -1,22 +1,18 @@
 class Conta:
-    def __init__ (self, nm_conta, saldo, registrar_transacoes):
+    def __init__(self, nm_conta, saldo):
         self.nm_conta = nm_conta
         self.saldo = saldo
-        self.registrar_transacoes = registrar_transacoes
         self.transacoes = []
-    
+
     def depositar(self, valor):
-        self.saldo = self.saldo +  valor
-        self.registrar_transacoes(f'Depósito de R${valor}')
-        
+        self.saldo += valor
+        self.transacoes.append(f'Depósito de R${valor}')
+
     def sacar(self, valor):
-        self.saldo = self.saldo + valor
-        self.registrar_transacoes(f'Saque de R${valor}')
-        
-    def registrar_transacoes(self, descricao):
-        self.transacoes.append(descricao)
-        
-    def exibir_extrato (self):
+        self.saldo -= valor
+        self.transacoes.append(f'Saque de R${valor}')
+
+    def exibir_extrato(self):
         print('Extrato da conta: ', self.nm_conta)
         print(' ')
         print('Transações: ')
@@ -24,6 +20,8 @@ class Conta:
         print(' ')
         print('Saldo atual: R$', self.saldo)
 
-valor = 12  
-conta01 = Conta(1, 1.000, " ")
-Conta.depositar(valor)
+valor = 12
+conta01 = Conta(1, 1000)
+Conta.sacar(conta01, valor)
+Conta.depositar(conta01, valor)
+Conta.exibir_extrato(conta01)
